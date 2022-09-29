a701b72def10e9473f9c25cef0dcbaaa797683c3
Bohdan Shakhov <b.shakhov@knubisoft.com>
2022-09-29 20:20:59 +0300

    init commit

---
+++ src/main/java/com/knubisoft/bmwtesttask/BmwTestTaskApplication.java
@@ -0,0 +1,11 @@
+package com.knubisoft.bmwtesttask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BmwTestTaskApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(BmwTestTaskApplication.class, args);
+    }
+}
